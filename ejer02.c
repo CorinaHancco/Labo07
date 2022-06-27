7d17ee99da6b3bc5035242e7cbd697aed0803b1d
Corina <chanccov@unsa.edu.pe>
2022-06-26 22:13:06 -0500

    Ejercicio 2, condicion terminada

--- ejer02.c
+++ ejer02.c
@@ -1,14 +1,13 @@
 #include <stdio.h>
 #include <inttypes.h>
  
-int64_t mult_ok(int x, int y) {
+int64_t mult_ok(int64_t x, int64_t y) {
   int64_t p = x*y;
-  /* x es 0, o dividir p por x = y */
-  return !x || p/x == y;
+  return !x || (p <= INT64_MAX) && (p > 0);
 }
 
 int main(void){
- int64_t resultado = mult_ok(500000,8);
+ int64_t resultado = mult_ok(500000000,8);
  printf("%" PRId64 "\n",resultado);
  return 0;
 }
