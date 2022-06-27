47c36336229552f7ff8fd3f32d5881cd02ca383b
Corina <chanccov@unsa.edu.pe>
2022-06-26 20:20:10 -0500

    plantilla ejercicio 01

---
+++ ejer01.c
@@ -0,0 +1,30 @@
+//#include <avr/io.h>
+#include <stdio.h>
+
+int bis(int x, int m){
+  return (x | (1 << (m-1)));
+}
+
+int bic(int x, int m){
+  return (x & (~(1 << (m - 1))));
+}
+
+/* Calcular x|y usando solo llamadas a las funciones bis y bic */
+/*int bool_or(int x, int y) {
+int resultado = __________ ;
+return resultado;
+}*/
+
+/* Calcula x^y usando solo llamadas a las funciones bis y bic */
+/*int bool_xor(int x, int y) {
+int resultado = __________;
+return resultado;
+}*/
+
+int main(void){
+  int r = bic(5,1);
+  int r2 = bis(10,2);
+  printf("%d\n",r);
+  printf("%d\n",r2);
+  return 0;
+}
